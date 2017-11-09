55e20885f1ab080706c91ba1452b93cdd70aba89
unknown <eduard.ljaschenko@web.de>
2017-11-09 18:17:51 +0100

    basic login/register gui hinzugefügt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegisterPage } from '../register/register';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openLogin() {
+  	this.navCtrl.push(LoginPage);
+  }
+
+  openRegister() {
+  	this.navCtrl.push(RegisterPage);
   }
 
 }
